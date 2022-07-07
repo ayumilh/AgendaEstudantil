69fa83b631c12e2d16861d9445ee002f17e84365
Lara Ayumi <83134763+ayumilh@users.noreply.github.com>
2022-07-06 21:45:32 -0300

    quando o send for clicado
    o envio do form quando chamado vai iniciar todo o codigo em seguida.
    ou seja, a partir do button["send"]

--- php/main.php
+++ php/main.php
@@ -1,21 +1,27 @@
 <?php
-//materia
-$sigla = $_POST[""]; //PK
-$disciplina = $_POST[""];
 
-//atividade
-$atv_data = $_POST[""];
-$atv_desc = $_POST[""];
-$foto = $_FILES[""];
-$link = $_POST[""];
-$observacao = $_POST[""];
-$entregar = $_POST[""];
-
-//prova
-$prova_data = $_POST[""];
-$conteudo = $_POST[""];
-$revisar = $_POST[""];
-$nota = $_POST[""];
+// verificar se o button com value["send"] foi clicado
+if(isset($_POST["send"])){
+  // MATERIA -> INSERT INTO
+  $sigla = $_POST[""]; //PK
+  $disciplina = $_POST[""];
 
 
+  // ATIVIDADE -> INSERT INTO
+  $atv_data = $_POST[""];
+  $atv_desc = $_POST[""];
+  $foto = $_FILES[""];
+  $link = $_POST[""];
+  $observacao = $_POST[""];
+  $entregar = $_POST[""];
+
+
+  //PROVA -> INSERT INTO
+  $prova_data = $_POST[""];
+  $conteudo = $_POST[""];
+  $revisar = $_POST[""];
+  $nota = $_POST[""];
+
+
+}
 ?>
