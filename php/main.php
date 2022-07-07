731e98178b81be71e9e9047fce846278761eb894
Lara Ayumi <83134763+ayumilh@users.noreply.github.com>
2022-07-06 21:14:02 -0300

    estrtutura da pagina da insercao das licoes

---
+++ php/main.php
@@ -0,0 +1,21 @@
+<?php
+//materia
+$sigla = $_POST[""]; //PK
+$disciplina = $_POST[""];
+
+//atividade
+$atv_data = $_POST[""];
+$atv_desc = $_POST[""];
+$foto = $_FILES[""];
+$link = $_POST[""];
+$observacao = $_POST[""];
+$entregar = $_POST[""];
+
+//prova
+$prova_data = $_POST[""];
+$conteudo = $_POST[""];
+$revisar = $_POST[""];
+$nota = $_POST[""];
+
+
+?>
