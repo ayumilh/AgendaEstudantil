a7df74eb61e2a58a7e9c355930fead73a3bf6959
Lara Ayumi <83134763+ayumilh@users.noreply.github.com>
2023-01-09 21:20:19 -0300

    btn chamando o inp do aside

--- javaScript/task.js
+++ javaScript/task.js
@@ -1,14 +1,34 @@
 // elementos da tarefa
-var cardTitle = document.querySelector(".js-card-title");
-var cardText = document.querySelector(".js-card-text");
+// var cardTitle = document.querySelector(".js-card-title");
+// var cardText = document.querySelector(".js-card-text");
 
-var btn = document.querySelector(".js-btn-fast");
+// var btn = document.querySelector(".js-btn-fast");
 
-btn.addEventListener('click', function(e){
-  e.preventDefault();   
-  cardTitle.innerHTML = "Quimica";
-  cardText.innerHTML = "estudar para prova";
+// btn.addEventListener('click', function(e){
+//   e.preventDefault();   
+//   cardTitle.innerHTML = "Quimica";
+//   cardText.innerHTML = "estudar para prova";
+// })
+
+// ativando e desativando input
+var btnCallInp = document.querySelector(".js-list-plus"); /* button que chama o input */
+var hide = document.querySelector(".hide");
+
+btnCallInp.addEventListener('click', function(e){
+  e.preventDefault();
+  hide.style.display = 'block';
 })
 
-// add lista de tarefas no nav-list
-var addList = document.querySelector(".js-list-plus");
+// inserindo na lista
+var btnAddList = document.querySelector(".btn-plus");
+var lista = document.querySelectorAll("ul li ul li");
+
+// pegando o texto do input
+var inputList = document.querySelector(".inp-list-plus");
+var titleList = inputList.value;
+
+
+btnAddList.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log(titleList);
+})
